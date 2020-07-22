1deec229b1a8d12eb245a7f90db1e1a47aea0215
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-21 21:20:33 -0400

    Update app.py to create template

--- app.py
+++ app.py
@@ -6,66 +6,126 @@ from flask import request
 from flask import url_for
 from flask import redirect
 from flask import request
-# from flask_pymongo import PyMongo
-
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 import datetime
 
-
+load_dotenv()
 
 # -- Initialization section --
 app = Flask(__name__)
 app.jinja_env.globals['current_time'] = datetime.datetime.now()
 
 
-events = [
-        {"name":"First Day of Classes", "date":"2020-08-21"},
-        {"name":"Winter Break", "date":"2020-12-20"},
-        {"name":"Finals Begin", "date":"2020-12-01"}
-    ]
-#
-# MONGO_DBNAME = ''
-# MONGO_DB_USERNAME = ''
-# MONGO_DB_PASSWORD = ''
-#
-# app.config['MONGO_DBNAME'] = MONGO_DBNAME
-# app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@cluster0-kxrbn.mongodb.net/{MONGO_DBNAME}?retryWrites=true'
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
 
-# mongo = PyMongo(app)
+app.config['MONGO_DBNAME'] = MONGO_DBNAME
+app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@cluster0.94cb3.mongodb.net/{MONGO_DBNAME}?retryWrites=true'
+
+mongo = PyMongo(app)
 
 # -- Routes section --
-# INDEX
 
 @app.route('/')
 @app.route('/index')
 def index():
     data = {
-    'events':events,
     }
     return render_template('index.html', data=data)
 
-@app.route('/view')
-def events_view():
+@app.route('/users')
+def users_view():
     data = {
-    'events':events,
+    'users':mongo.db.users.find({}),
     }
-    return render_template('eventsView.html', data=data)
+    return render_template('usersView.html', data=data)
 
-@app.route('/add', methods=['GET','POST'])
-def events_add():
+@app.route('/users/<USER>')
+def users_detail(USER):
+    data = {
+    'user':mongo.db.users.find_one({'name':USER}),
+    'reviews':mongo.db.reviews.find({'user':USER}),
+    }
+    return render_template('usersDetail.html', data=data)
+
+@app.route('/users/add', methods=['GET','POST'])
+def users_add():
     if request.method == 'GET':
         data = {
-
         }
-        return render_template('eventsAdd.html', data=data)
+        return render_template('usersAdd.html', data=data)
     else:
-        ## Add event to events_list
         form = request.form
-
-        event = {
-        'name':form['eventName'],
-        'date':form['eventDate'],
+        user = {
+        'name':form['userName'],
         }
+        mongo.db.users.insert(user)
+        return redirect(url_for('users_view'))
 
-        events.append(event)
+@app.route('/books')
+def books_view():
+    data = {
+    'books':mongo.db.books.find({}),
+    }
+    return render_template('booksView.html', data=data)
 
-        return redirect(url_for('events_view'))
+@app.route('/books/<title>')
+def books_detail(title):
+    data = {
+    'book':mongo.db.books.find_one({'title':title}),
+    'reviews':mongo.db.reviews.find({'title':title}),
+    }
+    return render_template('booksDetail.html', data=data)
+
+@app.route('/books/add', methods=['GET','POST'])
+def books_add():
+    if request.method == 'GET':
+        data = {
+        }
+        return render_template('booksAdd.html', data=data)
+    else:
+        form = request.form
+        book = {
+        'title':form['bookTitle'],
+        'author':form['bookAuthor'],
+        'genre':form['bookGenre'],
+        }
+        data = {
+        'book':book
+        }
+        mongo.db.books.insert(book)
+        return render_template('booksDetail.html', data=data)
+
+@app.route('/reviews')
+def reviews_view():
+    data = {
+    'reviews':mongo.db.reviews.find({}),
+    }
+    return render_template('reviewsView.html', data=data)
+
+@app.route('/review/add', methods=['GET','POST'])
+def reviews_add():
+    if request.method == 'GET':
+        data = {
+            'books':mongo.db.books.find({}),
+            'users':mongo.db.users.find({}),
+        }
+        return render_template('reviewsAdd.html', data=data)
+    else:
+        form = request.form
+        review = {
+        'title':form['bookTitle'],
+        'user':form['reviewUser'],
+        'rating':int(form['reviewRating']),
+        'review':form['reviewText'],
+        }
+        data = {
+        'review':review
+        }
+        mongo.db.reviews.insert(review)
+        return render_template('reviewsDetail.html', data=data)
+
+#### Add new routes below this line ###
--- readme.md
+++ readme.md
@@ -1,3 +1,23 @@
-# Flask + MongoDB Template
+# We Rate Books
 
-Clone this repository to get started building a Flask app with a MongoDB database.
+## Overview
+We will be building a website where users can add their name and upload books with book reviews.
+
+Your teacher will share credentials for MongoDB so we will all be working with the same data.
+
+## Your Goals
+You will be able to practice
+- Writing Flask Routes
+- Processing Forms
+- Inserting to a Database
+- Querying a Database
+- Incorporate Bootstrap Components
+- Style with Bootstrap Classes
+- Write custom CSS
+
+## Things to Include
+- Filter by Book Genre
+- Filter by book rating
+- Filter by User
+- Calculate average rating for a book
+- Calculate the [Net Promotor](https://en.wikipedia.org/wiki/Net_Promoter) score for a book
